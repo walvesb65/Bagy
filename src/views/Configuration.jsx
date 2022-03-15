d678b6ec0b650438f1be883910bef3cf75f6cf87
Willian Alves <willian.alvesb65@gmail.com>
2022-03-15 19:36:25 -0300

    Feat: Create navigation pages

---
+++ src/views/Configuration.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Configuration () {
+  return (
+    <>
+      <p>Configuration</p>
+    </>
+  );
+};
---
+++ src/views/Stores.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Stores () {
+  return (
+    <>
+      <p>Stores</p>
+    </>
+  );
+};
