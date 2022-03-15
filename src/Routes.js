e923e1d12606211382331776960eeebf56319ead
Willian Alves <willian.alvesb65@gmail.com>
2022-03-15 19:19:14 -0300

    Feat: Route configuration and clean React App

---
+++ src/Routes.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Routes , Route } from 'react-router-dom';
+import Overview from './views/overview';
+import Stores from './views/Stores';
+import Sales from './views/Sales';
+import Clients from './views/Clients';
+import Products from './views/Products';
+import Plans from './views/Plans'
+import Configuration from './views/Configuration'
+import Exit from './views/Exit';
+
+export default function Navigation () {
+  return (
+    <Routes>
+      <Route exact path="/" element={<Overview />} />
+      <Route exact path="/stores" element={<Stores />} />
+      <Route exact path="/sales" element={<Sales />} />
+      <Route exact path="/clients" element={<Clients />} />
+      <Route exact path="/products" element={<Products />} />
+      <Route exact path="/plans" element={<Plans />} />
+      <Route exact path="/configuration" element={<Configuration />} />
+      <Route exact path="/exit" element={<Exit />} />
+    </Routes>
+  );
+}
