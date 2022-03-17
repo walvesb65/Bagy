115c3de879074c80ddbfda78c211c9f241563a86
Willian Alves <willian.alvesb65@gmail.com>
2022-03-16 23:15:26 -0300

    Fix: Importing to overview page

--- src/views/Overview/index.jsx
+++ src/views/Overview/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import Header from '../../components/Header'
+import Header from '../../components/Header';
+import Datasection from '../../components/Datasection';
+import Chartsection from '../../components/Chartsection';
+import Reportsection from '../../components/Reportsection';
 import './styles.css';
 
 export default function Overview ({name}) {
   return (
     <div className='container_overview'>
       <Header namePage={name}/>
+      <Datasection />
+      <Chartsection />
+      <Reportsection />
     </div>
   );
 }
