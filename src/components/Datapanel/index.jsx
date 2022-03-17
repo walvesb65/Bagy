a38797087a14bde209e62eec46cb8cfffcb08630
Willian Alves <willian.alvesb65@gmail.com>
2022-03-16 23:28:37 -0300

    Feat: Configuring Datapanel component page + Styles

---
+++ src/components/Datapanel/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles.css';
+
+export default function Datapanel() {
+  return (
+    <div className='container_data_panel'>
+      <p>Test</p>
+      <span>10.0000</span>
+    </div>
+  );
+};
---
+++ src/components/Datasection/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Datapanel from '../Datapanel';
+import './styles.css'
+
+export default function Datasection() {
+
+  return (
+    <div className='container_data_session'>
+      <Datapanel  />
+      <Datapanel  />
+      <Datapanel  />
+      <Datapanel  />
+    </div>
+  );
+};
